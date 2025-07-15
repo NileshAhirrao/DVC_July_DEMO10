10d7e3396cabd0fdc20560ce3cbd22fc971f3088
nileshahirrao10 <nileshahirrao10@gmail.com>
2025-07-15 19:43:20 +0530

    DVC changes added

--- mycode.py
+++ mycode.py
@@ -3,9 +3,9 @@ import os
 
 
 data={
-    'NAME':['A','B','C'],
-    'AGE':[21,34,22],
-    'CLASS':['XII','XI','X']
+    'NAME':['A','B','C','D'],
+    'AGE':[21,34,22,19],
+    'CLASS':['XII','XI','X','XI']
 }
 
 
